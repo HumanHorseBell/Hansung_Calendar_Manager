3a2a4fe5789647bfe0b20cc29e6a8f8fa9e1a905
lovely2270 <lovely2270@naver.com>
2019-10-21 14:13:39 +0900

    add jhcode

---
+++ app/src/main/java/com/leehoyoon/computer/hansung_calendar_manager/JoinActivity.kt
@@ -0,0 +1,26 @@
+package com.example.kjhapplication
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_join.*
+import java.util.*
+
+
+/**
+ * Created by lovel on 2019-10-21.
+ */
+class JoinActivity : AppCompatActivity() {
+
+    public override fun onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+
+
+        //생일 DatePicker사용
+        birthcheckbtn.setOnClickListener {
+            val newFragment = BirthCheckFragment()   //DatePickerFragment 객체 생성
+            newFragment.show(getSupportFragmentManager(), "BirthCheck")                //프래그먼트 매니저를 이용하여 프래그먼트 보여주기
+        }
+    }
+}
---
+++ app/src/main/java/com/leehoyoon/computer/hansung_calendar_manager/LoginActivity.kt
@@ -0,0 +1,7 @@
+package com.example.kjhapplication
+
+/**
+ * Created by lovel on 2019-10-21.
+ */
+class LoginActivity {
+}
