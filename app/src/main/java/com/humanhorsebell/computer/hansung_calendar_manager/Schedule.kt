356ec17aece642fb8d9bdd49a6131bbe4f1dc627
HoYoon-Lee <happylhy0630@naver.com>
2019-12-03 16:22:54 +0900

    change Timeline Shape

---
+++ app/src/main/java/com/humanhorsebell/computer/hansung_calendar_manager/Schedule.kt
@@ -0,0 +1,3 @@
+package com.humanhorsebell.computer.hansung_calendar_manager
+
+class Schedule(val grpName : String, val startDate : String, val endDate : String, val picture : String?, val name : String)
