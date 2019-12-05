76279dcc9d3bbe0e96d0feb39e782a346f4b5a45
HoYoon-Lee <happylhy0630@naver.com>
2019-12-05 10:21:09 +0900

    load Schedule List

--- app/src/main/java/com/humanhorsebell/computer/hansung_calendar_manager/Schedule.kt
+++ app/src/main/java/com/humanhorsebell/computer/hansung_calendar_manager/Schedule.kt
@@ -1,3 +1,16 @@
 package com.humanhorsebell.computer.hansung_calendar_manager
 
-class Schedule(val grpName : String, val startDate : String, val endDate : String, val picture : String?, val name : String)
+data class Schedule(val grpName : String, val startDate : String, val endDate : String, val picture : String?, val name : String) : Comparable<Schedule>{
+    override fun compareTo(other: Schedule): Int {
+        /*for(i in 0..startDate.length){
+            if(startDate[i] > other.startDate[i]) return 1;
+            if(startDate[i] < other.startDate[i]) return -1;
+        }*/
+
+        if(startDate > other.startDate) return 1;
+        if(startDate < other.startDate) return -1;
+        if(name > other.name) return 1;
+        if(name < other.name) return -1;
+        else return 0;
+    }
+}
