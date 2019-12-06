36e754ec9c289aaaaa1aec462d160265771f861b
jinminilol <wlsalsrns@naver.com>
2019-12-06 15:48:32 +0900

    Merge branch 'master' of https://github.com/HumanHorseBell/Hansung_Calendar_Manager
    # Conflicts:
    #	app/src/main/java/com/humanhorsebell/computer/hansung_calendar_manager/EditActivity.kt
    #	app/src/main/res/layout/activity_calendar_dialog_add.xml

--- app/src/main/java/com/humanhorsebell/computer/hansung_calendar_manager/EditActivity.kt
+++ app/src/main/java/com/humanhorsebell/computer/hansung_calendar_manager/EditActivity.kt
@@ -14,23 +14,25 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_edit.*
-import org.jetbrains.anko.find
 import java.util.*
+
 import kotlin.collections.ArrayList
 
 
 class EditActivity : AppCompatActivity() {
     //login한 user의 데이터를 가져와 group 0-N 까지의 뒤져 grpMem에 속하면 schedule에 들어가 해당 날짜의 데이터 추출,,,,,여기서는 임의로 user2의 데이터 추출
-    val databaseUser = FirebaseDatabase.getInstance().reference.child("user")
+    val databaseUser = FirebaseDatabase.getInstance().getReference()
+
     val databaseGroup = FirebaseDatabase.getInstance().reference.child("group")
 
     var group: Group? = null;
 
     //임의로 user결정 및 group 결정
-    val curUser = "user0"
-    val curGroup = "0"
-    val database =  FirebaseDatabase.getInstance().reference.child("group").child(curGroup).child("schedule").child("2019-12-05")
+    val curUser = "0"
+    val curGroup = 0
+    val database = FirebaseDatabase.getInstance().reference.child("group").child(curGroup.toString()).child("schedule").child("2019-12-05")
     val todoName = ArrayList<String>()
+    var switch1: Switch? = null
     lateinit var myAdapter: BaseAdapter
 
     var grpMems = ArrayList<String>()
@@ -43,13 +45,15 @@ class EditActivity : AppCompatActivity() {
     var txtTitle: EditText? = null
     var txtDate: EditText? = null
     var txtTime: EditText? = null
+
+    var i: Int = 1
     private var mYear = 0
     private var mMonth = 0
     private var mDay = 0
     private var mHour = 0
     private var mMinute = 0
     private var asdf: String? = null
-
+    private var flag = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
@@ -83,13 +87,13 @@ class EditActivity : AppCompatActivity() {
     }
 
     var valueEventListener: ValueEventListener = object : ValueEventListener {
-        override fun onCancelled(p0: DatabaseError) { }
+        override fun onCancelled(p0: DatabaseError) {}
 
         override fun onDataChange(p0: DataSnapshot) {
-            for(data in p0.children) {
+            for (data in p0.children) {
                 val groupNum = data.key
                 val title = data.child("name")
-                if(groupNum != null) {
+                if (groupNum != null) {
                     todoName.add(groupNum.toString())
                     myAdapter.notifyDataSetChanged()
                     myAdapter.notifyDataSetInvalidated()
@@ -132,7 +136,7 @@ class EditActivity : AppCompatActivity() {
                 mMonth = c[Calendar.MONTH]
                 mDay = c[Calendar.DAY_OF_MONTH]
                 val datePickerDialog = DatePickerDialog(this.context,
-                        DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth -> txtDate!!.setText(dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year) }, mYear, mMonth, mDay)
+                        DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth -> txtDate!!.setText(year.toString()+ "-" +dayOfMonth.toString() + "-" + (monthOfYear + 1)) }, mYear, mMonth, mDay)
                 datePickerDialog.show()
             }
             btnTimePicker!!.setOnClickListener {
@@ -144,8 +148,44 @@ class EditActivity : AppCompatActivity() {
                         TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute -> txtTime!!.setText("$hourOfDay:$minute") }, mHour, mMinute, false)
                 timePickerDialog.show()
             }
+            /*btnDatePicker!!.setOnClickListener {
+                val c = Calendar.getInstance()
+                mYear = c[Calendar.YEAR]
+                mMonth = c[Calendar.MONTH]
+                mDay = c[Calendar.DAY_OF_MONTH]
+                val datePickerDialog = DatePickerDialog(this.context,
+                        DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth -> txtDate!!.setText(dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year) }, mYear, mMonth, mDay)
+                datePickerDialog.show()
+            }
+            btnTimePicker!!.setOnClickListener {
+                val c = Calendar.getInstance()
+                mHour = c[Calendar.HOUR_OF_DAY]
+                mMinute = c[Calendar.MINUTE]
+                // Launch Time Picker Dialog
+                val timePickerDialog = TimePickerDialog(this.context,
+                        TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute -> txtTime!!.setText("$hourOfDay:$minute") }, mHour, mMinute, false)
+                timePickerDialog.show()
+            }*/
             cancelBtn = findViewById<View>(R.id.btn_cancel) as Button
             ok = findViewById<View>(R.id.btn_ok) as Button
+            switch1 = findViewById(R.id.switch1)
+            switch1!!.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked) {
+                    flag = true
+                    btnDatePicker!!.visibility = View.GONE
+                    btnTimePicker!!.visibility = View.GONE
+                    txtDate!!.visibility = View.GONE
+                    txtTime!!.visibility = View.GONE
+
+                } else {
+                    flag = false
+                    btnDatePicker!!.visibility = View.VISIBLE
+                    btnTimePicker!!.visibility = View.VISIBLE
+                    txtDate!!.visibility = View.VISIBLE
+                    txtTime!!.visibility = View.VISIBLE
+
+                }
+            })
         }
         val dialog = AlertDialog.Builder(this)
                 .setView(view)
@@ -156,7 +196,33 @@ class EditActivity : AppCompatActivity() {
         }
         //ok눌렀을 때도 리스너 알맞게 달기
         ok!!.setOnClickListener {
+            if(flag==true) {
+
+            print(txtTitle)
             asdf = txtTitle!!.text.toString()
+            var database_jm = FirebaseDatabase.getInstance().reference
+            var jm=database_jm.child("group").child(curGroup.toString()).child("wishlist")
+
+            jm.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(dataSnapshot: DatabaseError) { }
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    for (data in dataSnapshot.children) {
+                        var wishListNum = data.key
+                    i++
+                    }
+                    jm.child("wishlist"+i).setValue(asdf!!)
+                    i=1
+                }
+            })
+//위시리스트
+}else{
+                var database_jm = FirebaseDatabase.getInstance().reference
+                var jm = database_jm.child("group").child(curGroup.toString())
+            //할일
+
+}
+
+
             /* val a = findViewById<View>(R.id.todoEditText) as EditText
              a.setText(asdf.toString()) */
             dialog.dismiss()
