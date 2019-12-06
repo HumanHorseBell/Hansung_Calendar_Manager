79a1f397710f7dbf607895ab264910ef4c12a302
HoYoon-Lee <happylhy0630@naver.com>
2019-12-07 06:48:15 +0900

    add schedule activity

--- app/src/main/java/com/humanhorsebell/computer/hansung_calendar_manager/ScheduleActivity.kt
+++ app/src/main/java/com/humanhorsebell/computer/hansung_calendar_manager/ScheduleActivity.kt
@@ -1,12 +1,23 @@
 package com.humanhorsebell.computer.hansung_calendar_manager
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import com.bumptech.glide.load.engine.Resource
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_schedule.*
+import org.jetbrains.anko.Android
+import org.jetbrains.anko.selectorResource
+import java.nio.channels.Selector
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class ScheduleActivity : AppCompatActivity() {
     lateinit var userNo:String
@@ -15,20 +26,45 @@ class ScheduleActivity : AppCompatActivity() {
     lateinit var scheduleNo: String
     val comments = ArrayList<Comment>()
     lateinit var scheduleListAdapter: ScheduleListAdapter
+    lateinit var count: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_schedule)
 
+        userNo = "3"
         groupNo = "0"
         day = "2019-10-20"
         scheduleNo = "0"
 
-        scheduleListAdapter = ScheduleListAdapter(applicationContext, comments)
+        scheduleListAdapter = ScheduleListAdapter(applicationContext, comments, userNo)
         listViewComment.adapter = scheduleListAdapter
+        listViewComment.divider = null
+        //listViewComment.isEnabled = false
 
         val firebaseDatabase = FirebaseDatabase.getInstance().reference
                 .child("group").child(groupNo).child("schedule").child(day).child(scheduleNo)
+
+        val commentListener = object : ValueEventListener {
+            override fun onCancelled(databaseError: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                comments.clear()
+                val children = dataSnapshot.children
+                for (child in children) {
+                    val message = child.child("data").value.toString()
+                    val writer = child.child("writer").value.toString()
+                    val mDate = child.child("mDate").value.toString()
+                    val mTime = child.child("mTime").value.toString()
+
+                    comments.add(Comment(message, mDate, mTime, "", writer))
+                    scheduleListAdapter.notifyDataSetChanged()
+                    scheduleListAdapter.notifyDataSetInvalidated()
+                }
+            }
+        }
 
         val singleGroupScheduleListener = object : ValueEventListener {
             override fun onCancelled(databaseError: DatabaseError) {
@@ -43,19 +79,52 @@ class ScheduleActivity : AppCompatActivity() {
                 textViewEndDay.text = dataSnapshot.child("endDate").value.toString()
                 textViewEndTime.text = dataSnapshot.child("endTime").value.toString()
 
-                val children = dataSnapshot.child("comment").children
-                for(child in children){
-                    val message = child.child("data").value.toString()
-                    val writer = child.child("writer").value.toString()
-                    val mDate = child.child("mDate").value.toString()
-                    val mTime = child.child("mTime").value.toString()
-
-                    comments.add(Comment(message, mDate, mTime, "", writer))
-                    scheduleListAdapter.notifyDataSetChanged()
-                    scheduleListAdapter.notifyDataSetInvalidated()
-                }
+                firebaseDatabase.child("comment").addValueEventListener(commentListener)
             }
         }
         firebaseDatabase.addListenerForSingleValueEvent(singleGroupScheduleListener)
+
+        val singleCommentNumberListener = object : ValueEventListener {
+            override fun onCancelled(databaseError: DatabaseError) {
+
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                count = dataSnapshot.children.count().toString()
+            }
+        }
+        firebaseDatabase.child("comment").addListenerForSingleValueEvent(singleCommentNumberListener)
+
+        val editListener = object : TextView.OnEditorActionListener{
+            override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+                when(actionId){
+                    EditorInfo.IME_ACTION_SEND -> buttonSubmit.performClick()
+                    else -> return false
+                }
+                return true
+            }
+        }
+        editTextComment.setOnEditorActionListener(editListener)
+
+        val buttonClickListener = View.OnClickListener{v ->
+            if(!editTextComment.text.isEmpty()){
+                var comment = firebaseDatabase.child("comment").child(count)
+
+                comment.child("data").setValue(editTextComment.text.toString())
+                comment.child("writer").setValue(userNo)
+
+                val now = System.currentTimeMillis()
+                val date = Date(now)
+                val formatterDate = SimpleDateFormat("yyyy-MM-dd")
+                val formatterTime = SimpleDateFormat("HH:mm")
+                val formattedDate = formatterDate.format(date)
+                val formattedTime = formatterTime.format(date)
+
+                comment.child("mDate").setValue(formattedDate)
+                comment.child("mTime").setValue(formattedTime)
+                editTextComment.setText("")
+            }
+        }
+        buttonSubmit.setOnClickListener(buttonClickListener)
     }
 }
--- app/src/main/java/com/humanhorsebell/computer/hansung_calendar_manager/ScheduleListAdapter.kt
+++ app/src/main/java/com/humanhorsebell/computer/hansung_calendar_manager/ScheduleListAdapter.kt
@@ -1,24 +1,54 @@
 package com.humanhorsebell.computer.hansung_calendar_manager
 
 import android.content.Context
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.LinearLayout
 import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.schedule_list_view_item.view.*
 
-class ScheduleListAdapter(val context: Context, val comments: ArrayList<Comment>) : BaseAdapter() {
+class ScheduleListAdapter(val context: Context, val comments: ArrayList<Comment>, val userNo: String) : BaseAdapter() {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view = LayoutInflater.from(context).inflate(R.layout.schedule_list_view_item, null) as View
+
+        view.isEnabled = false
 
         val writer = view.findViewById<TextView>(R.id.textViewWriter)
         val message = view.findViewById<TextView>(R.id.textViewMessage)
         val dayAndTime = view.findViewById<TextView>(R.id.textViewTime)
 
-        writer.text = comments[position].writer
+        val singleUserListener = object : ValueEventListener {
+            override fun onCancelled(databaseError: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                writer.text = dataSnapshot.child("name").value.toString()
+            }
+        }
+
+        if(userNo == comments[position].writer){
+            view.linearLayoutMessage.removeView(writer)
+            val linearLayoutItem = view.findViewById<LinearLayout>(R.id.linearLayoutItem)
+            val layoutParams = linearLayoutItem.layoutParams as LinearLayout.LayoutParams
+            layoutParams.gravity = Gravity.RIGHT
+
+            view.linearLayoutMessage.gravity = Gravity.RIGHT
+            view.linearLayoutInfo.gravity = Gravity.LEFT
+        }else {
+            writer.setBackgroundResource(R.drawable.writer_layout)
+            //writer.text = comments[position].writer
+            FirebaseDatabase.getInstance().reference.child("user").child(comments[position].writer).addListenerForSingleValueEvent(singleUserListener)
+        }
         message.text = comments[position].data
-        dayAndTime.text = comments[position].mDate + " " + comments[position].mTime
+        dayAndTime.text = comments[position].mDate + "  " + comments[position].mTime
 
         return view
     }
